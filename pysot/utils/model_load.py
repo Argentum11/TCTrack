2749e82d90fc894b356c1d3df895c719cfa25aea
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2025-11-08 22:03:41 +0800

    fix: add weights_only=False to torch.load for compatibility with torch 2.9.0

--- pysot/utils/model_load.py
+++ pysot/utils/model_load.py
@@ -48,7 +48,7 @@ def load_pretrain(model, pretrained_path):
     logger.info('load pretrained model from {}'.format(pretrained_path))
     device = torch.cuda.current_device()
     pretrained_dict = torch.load(pretrained_path,
-        map_location=lambda storage, loc: storage.cuda(device))
+        map_location=lambda storage, loc: storage.cuda(device), weights_only=False)
     if "state_dict" in pretrained_dict.keys():
         pretrained_dict = remove_prefix(pretrained_dict['state_dict'],
                                         'module.')
